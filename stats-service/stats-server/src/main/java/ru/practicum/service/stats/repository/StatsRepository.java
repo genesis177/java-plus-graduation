a46a4002daed35c295ebecd8efaede43c7877473
Olya <alinagdl1@yandex.ru>
2026-01-15 19:16:10 +0500

    Revert "Первая проверка дипломного задания"

--- stats-service/stats-server/src/main/java/ru/practicum/service/stats/repository/StatsRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.service.stats.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.practicum.service.stats.model.Stats;
-
-public interface StatsRepository extends JpaRepository<Stats, Long> {
-}
